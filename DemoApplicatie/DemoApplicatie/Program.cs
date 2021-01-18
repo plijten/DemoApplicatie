eb604b04431ea52b05e4c13e31c689f3a131799a
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-18 10:29:31 +0100

    Added exception Handling

--- DemoApplicatie/DemoApplicatie/Program.cs
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -6,10 +6,19 @@ namespace DemoApplicatie
     {
         static void Main(string[] args)
         {
-            double number1 = Convert.ToDouble(Console.ReadLine());
-            double number2 = Convert.ToDouble(Console.ReadLine());
+            double number1;
+            double number2;
 
-            Console.WriteLine("Uitkomst is: {0}", number1 + number2);
+            Console.WriteLine("Getal 1");
+            bool x = Double.TryParse(Console.ReadLine(), out number1);
+
+            Console.WriteLine("Getal 2");
+            bool y = Double.TryParse(Console.ReadLine(), out number2);
+
+            if (x && y)
+                Console.WriteLine("Uitkomst is: {0}", number1 + number2);
+            else
+                Console.WriteLine("Invoer is ongeldig.");
         }
     }
 }
