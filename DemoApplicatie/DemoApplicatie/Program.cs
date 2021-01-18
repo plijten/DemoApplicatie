c85270116213bcddfc248386db422c161f349075
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-18 10:35:31 +0100

    naamgeving aangepast.

--- DemoApplicatie/DemoApplicatie/Program.cs
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -10,12 +10,12 @@ namespace DemoApplicatie
             double number2;
 
             Console.WriteLine("Getal 1");
-            bool x = Double.TryParse(Console.ReadLine(), out number1);
+            bool value1Correct = Double.TryParse(Console.ReadLine(), out number1);
 
             Console.WriteLine("Getal 2");
-            bool y = Double.TryParse(Console.ReadLine(), out number2);
+            bool value2Correct = Double.TryParse(Console.ReadLine(), out number2);
 
-            if (x && y)
+            if (value1Correct && value2Correct)
                 Console.WriteLine("Uitkomst is: {0}", number1 + number2);
             else
                 Console.WriteLine("Invoer is ongeldig.");
