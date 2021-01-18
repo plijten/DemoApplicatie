330060809bd775a4792056ca73e6e86d6c743725
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-18 08:57:26 +0100

    Created the project

---
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
