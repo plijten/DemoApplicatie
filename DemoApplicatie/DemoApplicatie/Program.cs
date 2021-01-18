e2e542f92b4ae943d94a4206d9f4591f060c3997
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-18 09:32:08 +0100

    Som toegevoegd.
    Nog geen foutafhandeling, dat doe ik later.

--- DemoApplicatie/DemoApplicatie/Program.cs
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -5,8 +5,11 @@ namespace DemoApplicatie
     class Program
     {
         static void Main(string[] args)
-        {            
-            Console.WriteLine("Hello Student!");
+        {
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Uitkomst is: {0}", number1 + number2);
         }
     }
 }
