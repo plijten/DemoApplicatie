7e7b0bca83175fe9d6ce9e3d9e571dffe4b6e999
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-18 09:17:10 +0100

    Changed the text.

--- DemoApplicatie/DemoApplicatie/Program.cs
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -5,8 +5,8 @@ namespace DemoApplicatie
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            Console.WriteLine("Hello Student!");
         }
     }
 }
