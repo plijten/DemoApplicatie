483fc31101c904e241d776813b2f9dc62d7c7df5
Peter Lijten <PLN02@rocvantwente.nl>
2021-01-21 11:07:25 +0100

    added multiply function

--- DemoApplicatie/DemoApplicatie/Program.cs
+++ DemoApplicatie/DemoApplicatie/Program.cs
@@ -6,10 +6,25 @@ namespace DemoApplicatie
     {
         static void Main(string[] args)
         {
-            double number1 = Convert.ToDouble(Console.ReadLine());
-            double number2 = Convert.ToDouble(Console.ReadLine());
+            double number1;
+            double number2;
 
-            Console.WriteLine("Uitkomst is: {0}", number1 + number2);
+            Console.WriteLine("Getal 1");
+            bool value1Correct = Double.TryParse(Console.ReadLine(), out number1);
+
+            Console.WriteLine("Getal 2");
+            bool value2Correct = Double.TryParse(Console.ReadLine(), out number2);
+
+            if (value1Correct && value2Correct)
+                Console.WriteLine("Uitkomst is: {0}", number1 + number2);
+            else
+                Console.WriteLine("Invoer is ongeldig.");
+        }
+
+        private void Multiply(double number1, double number2)
+        {
+            // Just for demo.
+            Console.WriteLine(number1 * number2);
         }
     }
 }
